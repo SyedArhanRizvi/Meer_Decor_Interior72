446b104716f31c481f11fe08149ed21e6a52cff1
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2024-08-03 12:10:28 +0530

    Re-again dollar

--- src/components/cartdesign/CartDesign.jsx
+++ src/components/cartdesign/CartDesign.jsx
@@ -8,7 +8,7 @@ function CartDesign({name, img, price}) {
     <div className='itemCartCard'>
       <div className="img"><img src={img} alt="" /></div>
       <div className="itemCartDetails">
-        <div className="itemCartPrice">Price is {price}</div>
+        <div className="itemCartPrice">Price is ${price}</div>
         <div className="itemCartName">{name}</div>
         <div className="itemCartDesc"><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error sapiente, tempore eum iusto eligendi pariatur, vel iste distinctio quae ipsum ratione recusandae quibusdam, illo ipsam fugit minus architecto atque. Voluptatem!</p></div>
 
